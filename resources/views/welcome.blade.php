14f61e66e2b9c21eb43bc2c29cde6db04147cc93
Anusurya S <anusurya11rr@gmail.com>
2022-08-13 12:26:00 +0530

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Anu Restarunt</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA==" crossorigin="anonymous" />
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-image: url("https://qul.imgix.net/a906bebb-d41d-4966-8e28-45ae80d24a3d/222213_landscape.jpg");
+                background-color: #d5f4e6;/* Used if the image is unavailable */
+  height: 500px; /* You must set a specified height */
+  background-repeat: no-repeat;
+  background-attachment: fixed;  
+  background-size: cover;
+                
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #ff0000;
+                padding: 0 25px;
+                font-size: 20px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+                font-size: 100px;
+            }
+        </style>
+    </head>
+    <body>
+    <img src="food.jpg" >
+        <div class="flex-center position-ref full-height">
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    @auth
+                        <a href="{{ url('/home') }}">Account</a>
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
+                    @endauth
+                </div>
+            @endif
+
+            <div class="content">
+                <div class="title m-b-md">
+                   <i class="fas fa-hamburger"></i><b> Anu Restaurant </b>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
